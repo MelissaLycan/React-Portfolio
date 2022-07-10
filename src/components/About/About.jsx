91e46dff2b1bf1981ff57f7d69e88fbb1d917f35
Melissa Lycan <melissamlycan@gmail.com>
2022-07-10 14:00:36 -0600

    About Me completed

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './about.css';
-import ME from "../../assets/images/Melissa-Lycan-Professional.jpg";
+import ME from "../../assets/images/Nordic-Daughter-074.jpg";
 import {FaAward} from 'react-icons/fa';
 import {FiUsers} from 'react-icons/fi';
 import {VscFolderLibrary} from 'react-icons/vsc';
@@ -9,9 +9,10 @@ import {VscFolderLibrary} from 'react-icons/vsc';
 export function About() {
   return (
     <section id='about'>
-    <h5>Get to Know</h5>
-      <h2>About Me</h2>
-
+      <div className="center">
+        <h5>Get to Know</h5>
+        <h2>About Me</h2>
+      </div>
       <div className="container about_container">
         <div className="about_me">
           <div className="about-image">
@@ -25,51 +26,45 @@ export function About() {
               <FaAward className='about_icon'/>
               <h5>Experience</h5>
               <small>20+ Years Working</small>
-
             </article>
 
              <article className='about_card'>
               <FiUsers className='about_icon'/>
               <h5>Clients</h5>
               <small>100+ Creative Projects</small>
-
             </article>
 
              <article className='about_card'>
               <VscFolderLibrary className='about_icon'/>
               <h5>Projects</h5>
               <small>Coding and Tech</small>
-
             </article>
+            </div>
+            <div>
+              <p>
+              I have worked most of my life in the private sector in Sales and
+              Management. After repeated success my passions took me into the music
+              business. Over a decade in music and a Bachelor of Science in Music
+              Business and I began to see trends everywhere. This sparked a passion
+              inside me for BigDATA and I raced to attain a Masters in BI, where I
+              discovered my love for math, strategy and logic all over again. Over the
+              last 5 years I have taught HS Math in Denver. Currently I am honing my
+              development skills and finishing certification in Full Stack Development
+              so that I may build my own concepts and contribute to the greater
+              accomplishments of mankind with my skills and talents.  I believe in people and their incredible ability to be blissfully
+              vulnerable and intensely strong all in the same moment. I believe
+              creativity is the ultimate expression of this lifetime, whether it is
+              coding, music, writing, painting or whatever you so choose. I am driven
+              by unconditional love, a desire to constantly accept feedback that will
+              improve my character and perspective, and a wanderlust for adventure and
+              exploration that can only be attributed to a deep seeded ancestral
+              calling.
+            </p>
+
+            <a href="#contact" className="btn btn-primary">Let's Talk</a>
+            </div>
+            </div>
           </div>
-        </div>
-        <br/>
-        <p>
-        I have worked most of my life in the private sector in Sales and
-        Management. After repeated success my passions took me into the music
-        business. Over a decade in music and a Bachelor of Science in Music
-        Business and I began to see trends everywhere. This sparked a passion
-        inside me for BigDATA and I raced to attain a Masters in BI, where I
-        discovered my love for math, strategy and logic all over again. Over the
-        last 5 years I have taught HS Math in Denver. Currently I am honing my
-        development skills and finishing certification in Full Stack Development
-        so that I may build my own concepts and contribute to the greater
-        accomplishments of mankind with my skills and talents.
-      </p><br/>
-      <p>
-        I believe in people and their incredible ability to be blissfully
-        vulnerable and intensely strong all in the same moment. I believe
-        creativity is the ultimate expression of this lifetime, whether it is
-        coding, music, writing, painting or whatever you so choose. I am driven
-        by unconditional love, a desire to constantly accept feedback that will
-        improve my character and perspective, and a wanderlust for adventure and
-        exploration that can only be attributed to a deep seeded ancestral
-        calling.
-      </p>
-
-      <a href="#contact" className="btn btn-primary">Let's Talk</a>
-      </div>
-
     </section>
   );
 }
