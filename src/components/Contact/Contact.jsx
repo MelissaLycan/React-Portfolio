fd16b9182913de47a7cd28793aa8bce4459bd9b7
Melissa Lycan <melissamlycan@gmail.com>
2022-07-11 00:06:42 -0600

    added Contact Form and Projects.  FInalizing my Read Me before final push

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,44 +1,45 @@
-import React, { useState } from "react";
+import React from "react";
 import "./contact.css";
+import {MdOutlineEmail, MdSms} from "react-icons/md"
+import { RiMessengerLine } from "react-icons/ri"
 
 const Contact = () => {
-  const [status, setStatus] = useState("Submit");
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setStatus("Sending...");
-    const { name, email, message } = e.target.elements;
-    let details = {
-      name: name.value,
-      email: email.value,
-      message: message.value,
-    };
-    let response = await fetch("http://localhost:5000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(details),
-    });
-    setStatus("Submit");
-    let result = await response.json();
-    alert(result.status);
-  };
+ 
   return (
-    <form id="contact" onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" required />
+    <section id="contact" className=" container">
+      <h5 className="center">Get in Touch</h5>
+      <h2 className="center">Contact Me</h2>
+      <div className="contact_container">
+      <div className="contact_options">
+        <article className="contact_option">
+        <MdOutlineEmail />
+        <h4>Email</h4>
+        <h5>melissamlycan@gmail.com</h5>
+        <a href="mailto:melissamlycan@gmail.com">Send a Message!</a>
+        </article>
+        <article className="contact_option">
+          <RiMessengerLine />
+        <h4>Messenger</h4>
+        <h5>Facebook</h5>
+        <a href="https://m.me/melissalycan">Facebook Me!</a>
+        </article>
+        <article className="contact_option">
+        <MdSms />
+        <h4>Text Message</h4>
+        <h5>(720)933-4868</h5>
+        <a href="sms:+17209334868">Text Me!</a>
+        </article>
       </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" required />
+      <div className="contact_form">
+      <form className="form" actions="">
+        <input className="inputName" type="text" name="name" placeholder='Your Full Name' required/>
+        <input className="inputEmail" type="email" name='email' placeholder="Your Email" required />
+        <textarea className="inputText" name="message" id="" rows="7" placeholder="Your Message" required></textarea>
+        <button type="submit" className='btn btn-primary'>Send Message</button>
+      </form>
       </div>
-      <div>
-        <label htmlFor="message">Message:</label>
-        <textarea id="message" required />
       </div>
-      <button type="submit">{status}</button>
-    </form>
+    </section>
   );
 };
 
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./projects.css"
-import img1 from "../../assets/images/linkedjazz2.jpg";
+import img1 from "../../assets/images/Melissa-Lycan-logo.gif";
 import img2 from "../../assets/images/img2.png";
 import img3 from "../../assets/images/img3.png";
 import img4 from "../../assets/images/img4.png";
@@ -11,38 +11,43 @@ const data = [
   {
     id:1,
     image: img5,
+    title: "CSS/Javascript NoteTaker",
     github: 'https://github.com/MelissaLycan/Note-Taker.git',
     demo: 'https://melissalycan.github.io/Note-Taker/'
   },
   {
     id:2,
     image: img2,
+    title: "TimeTrackerApp (Javascript)",
     github: 'https://github.com/MelissaLycan/Challenge-05.git',
     demo: 'https://melissalycan.github.io/Challenge-05/'
   },
   {
     id:3,
     image: img3,
+    title: "WeatherAPI (API's)",
     github: 'https://github.com/MelissaLycan/Challenge-06.git',
     demo: 'https://melissalycan.github.io/Challenge-06/'
   },
   {
     id:4,
     image: img4,
+    title: "InvetoryManagement (SQL)",
     github: 'https://github.com/MelissaLycan/Inventory-Management-App.git',
     demo: 'https://dry-caverns-71259.herokuapp.com'
   },
   {
     id:5,
-    image: img1,
-    github: 'https://github.com/MelissaLycan',
-    demo: 'https://www.melissalycan.com'
-  },
-  {
-    id:6,
     image: img6,
+    title: "BlogApplication (Node.js)",
     github: 'https://github.com/MelissaLycan/Blog-Application.git',
     demo: 'https://murmuring-ravine-73825.herokuapp.com/'
+  }, {
+    id:6,
+    image: img1,
+    title: "Creative/Projects (Website)",
+    github: 'https://github.com/MelissaLycan',
+    demo: 'https://www.melissalycan.com'
   }
   
   
@@ -57,16 +62,20 @@ export default function Projects() {
       <h5>My Projects</h5>
       <h2>Portfolio</h2>
 
-      <div className="container portfolio_container">
+      <div className="container project_container">
         {
           data.map(({ id, image, title, github, demo })=> {
             return( <article key={id} className="project_item">
+              <div className="container">
+              <div className="project-title center"><h3>{title}</h3></div>
           <div className="project-image"><img src={image} alt={title}/></div>
-          <h3>{title}</h3>
+          
           <div className="project_item-call">
           <a href={github} className='btn' target="_blank" rel="noreferrer">Github</a>
           <a href={demo} className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
           </div>
+          </div>
+          
         </article>
         )
           })
